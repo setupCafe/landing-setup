b08f5a16a761e5bffea1dd40c780bab8b9c56649
francisco <eduardo1978m2@gmil.com>
2019-05-11 19:44:28 -0500

    primer commit

---
+++ src/index.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const app = express();
+const path = require('path');
+
+
+
+
+//settings
+app.set('port', 4000);
+app.set('views', path.join(__dirname, 'views'));
+app.engine('html', require('ejs').renderFile);
+app.set('view engine', 'ejs');
+
+//middlewares
+
+//routes
+app.use(require('./routes/index'));
+
+//statics files
+app.use(express.static (path.join(__dirname, 'public')))
+
+
+// listening the server
+ app.listen(app.get('port'), () =>{
+    console.log ('Server on port', app.get('port'));
+ });
